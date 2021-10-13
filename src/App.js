8fcf47a3b08bff139d0cde7ae87a2e27bb9d9684
Lown Kim <wideplate3@gmail.com>
2021-10-13 15:04:13 +0900

    Add: 글목록 생성

---
+++ src/App.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  let [글제목, 글제목변경] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "파이썬 독학",
+  ]);
+  let [따봉, 따봉변경] = useState(0);
+  let posts = "강남 고기 맛집";
+
+  const 제목바꾸기 = () => {
+    const newArray = [...글제목];
+    newArray[0] = "여자 코트 추천";
+    글제목변경(newArray);
+  };
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <button onClick={제목바꾸기}>버튼</button>
+      <div className="list">
+        <h3>
+          {글제목[0]}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            💜
+          </span>
+          {따봉}
+        </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {글제목[1]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {글제목[2]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+    </div>
+  );
+}
+
+export default App;
