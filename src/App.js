12641dda4b9422f3840a19c4c8e0253183e22823
Lown Kim <wideplate3@gmail.com>
2021-10-14 18:21:56 +0900

    Add: make list, add list

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  let [글제목, 글제목변경] = useState([
-    "남자 코트 추천",
-    "강남 우동 맛집",
-    "파이썬 독학",
+  let [글정보, 글정보변경] = useState([
+    {
+      글제목: "남자 코트 추천",
+      하트: 0,
+    },
+    {
+      글제목: "강남 우동 맛집",
+      하트: 0,
+    },
+    {
+      글제목: "파이썬 독학",
+      하트: 0,
+    },
   ]);
-  let [따봉, 따봉변경] = useState(0);
-  let posts = "강남 고기 맛집";
 
-  const 제목바꾸기 = () => {
-    const newArray = [...글제목];
-    newArray[0] = "여자 코트 추천";
-    글제목변경(newArray);
+  let [modal, modal변경] = useState(true);
+  let [누른글정보, 누른글정보변경] = useState(0);
+  let [입력값, 입력값변경] = useState("");
+
+  const 하트올리기 = (i) => {
+    const newArr = 글정보.map((글) => {
+      if (글.글제목 === 글정보[i].글제목) {
+        return { ...글, 하트: 글.하트 + 1 };
+      } else {
+        return 글;
+      }
+    });
+    글정보변경(newArr);
   };
 
   return (
@@ -23,34 +39,64 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={제목바꾸기}>버튼</button>
-      <div className="list">
-        <h3>
-          {글제목[0]}
-          <span
-            onClick={() => {
-              따봉변경(따봉 + 1);
-            }}
-          >
-            💜
-          </span>
-          {따봉}
-        </h3>
-        <p>2월 17일 발행</p>
-        <hr />
+      {글정보.map((글, i) => {
+        return (
+          <div className="list" key={i}>
+            <h3
+              onClick={() => {
+                누른글정보변경(i);
+              }}
+            >
+              {글.글제목}
+              <span onClick={() => 하트올리기(i)}> 💜</span>
+              {글.하트}
+            </h3>
+            <p>10월 13일 발행</p>
+            <hr />
+          </div>
+        );
+      })}
+
+      <div className="publish">
+        <input
+          onChange={(e) => {
+            입력값변경(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            const arrayCopy = [...글정보];
+            arrayCopy.unshift({ 글제목: 입력값, 하트: 0 });
+            글정보변경(arrayCopy);
+            // console.log(arrayCopy);
+          }}
+        >
+          저장
+        </button>
       </div>
-      <div className="list">
-        <h3> {글제목[1]} </h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3> {글제목[2]} </h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
+
+      <button
+        onClick={() => {
+          modal === false ? modal변경(true) : modal변경(false);
+        }}
+      >
+        열고 닫기
+      </button>
+      {modal === true ? (
+        <Modal 글정보={글정보} 누른글정보={누른글정보} />
+      ) : null}
     </div>
   );
 }
+
+const Modal = (props) => {
+  return (
+    <div className="modal">
+      <h2>{props.글정보[props.누른글정보].글제목}</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+};
 
 export default App;
